be5855a17da024b2b8e1e164669afe45b670cee1
Francisco Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2022-10-30 21:34:35 +0000

    update Get Games From API

--- rasbet-backend/src/main/java/net/java/rasbetbackend/controller/GamesController.java
+++ rasbet-backend/src/main/java/net/java/rasbetbackend/controller/GamesController.java
@@ -1,16 +1,21 @@
 package net.java.rasbetbackend.controller;
 
 import net.java.rasbetbackend.model.*;
+import net.java.rasbetbackend.payload.request.APIRequest;
 import net.java.rasbetbackend.payload.response.MessageResponse;
 import net.java.rasbetbackend.repository.*;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -95,4 +100,39 @@ public class GamesController {
                 .badRequest()
                 .body(new MessageResponse("Error: Unknown category!"));
     }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasRole('ROLE_BETTER') or hasRole('ROLE_SPECIALIST') or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> updateGames(@Valid @RequestBody APIRequest apiRequest){
+        try {
+            URL url = new URL("http://ucras.di.uminho.pt/v1/games");
+            HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if (connection.getResponseCode() == 200){
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+                while(scanner.hasNext()){
+                    informationString.append(scanner.nextLine());
+                }
+                scanner.close();
+                JSONParser parse = new JSONParser(String.valueOf(informationString));
+                JSONArray dataObject = (JSONArray) parse.parse();
+                //for (JSONObject game : dataObject.get())
+                for (int i = 0; i<dataObject.size(); i++){
+                    JSONObject game = (JSONObject) dataObject.get(i);
+                    //Game g = new Game(game.get("id"),game.get(""));
+                    //gameRepository.save(game);
+                }
+            } else{
+                //Criar uma Exception
+                System.out.println("Erro!!!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
