3ccf8391e2180f88361eb248e57e4d88e7e248d7
Francisco Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2022-11-01 19:24:43 +0000

    update Sales e StateBet

---
+++ rasbet-backend/src/main/java/net/java/rasbetbackend/controller/BetController.java
@@ -0,0 +1,41 @@
+package net.java.rasbetbackend.controller;
+
+import net.java.rasbetbackend.model.Bet;
+import net.java.rasbetbackend.payload.response.MessageResponse;
+import net.java.rasbetbackend.repository.BetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/bets")
+public class BetController {
+
+    @Autowired
+    BetRepository betRepository;
+
+    @PostMapping("/change_state")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> changeState(String state, int idBet){
+        if(!betRepository.existsByIdBet(idBet)){ return ResponseEntity.badRequest().body(new MessageResponse("Error: Bet Non-Existing!")); }
+        try{
+            Optional<Bet> bet = betRepository.findByIdBet(idBet);
+            if(bet.isPresent()){
+                Bet be=bet.get();
+                be.setState(Bet.Bet_State.valueOf(state));
+                betRepository.save(be);
+            }
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
+        return ResponseEntity.ok(new MessageResponse("Bet State changed successfully!"));
+    }
+
+}
--- rasbet-backend/src/main/java/net/java/rasbetbackend/controller/SalesController.java
+++ rasbet-backend/src/main/java/net/java/rasbetbackend/controller/SalesController.java
@@ -1,16 +1,65 @@
 package net.java.rasbetbackend.controller;
 
+import net.java.rasbetbackend.model.Game;
+import net.java.rasbetbackend.model.Sale;
+import net.java.rasbetbackend.payload.request.SalesRequest;
+import net.java.rasbetbackend.payload.response.MessageResponse;
+import net.java.rasbetbackend.repository.GameRepository;
+import net.java.rasbetbackend.repository.SaleRepository;
+import net.java.rasbetbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/sales")
 public class SalesController {
 
-    //@Autowired
-    //SalesRepository salesRepository
+    @Autowired
+    SaleRepository saleRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    GameRepository gamesRepository;
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> createSale(@Valid @RequestBody SalesRequest salesRequest){
+        boolean all_games = false;
+        if (salesRequest.getIdGame() == -1){
+            all_games = true;
+        }
+        else if(!gamesRepository.existsByIdGame(salesRequest.getIdGame())){
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Game Non-Existing!"));
+        }
+        try{
+            if(all_games){
+                for (Game game: gamesRepository.findAll()){
+                    Sale sale = new Sale(
+                            game.getIdGame(),
+                            salesRequest.getValidationTime(),
+                            Sale.Sale_State.Available
+                    );
+                    saleRepository.save(sale);
+                }
+            }
+            else{
+                Sale sale = new Sale(
+                        salesRequest.getIdGame(),
+                        salesRequest.getValidationTime(),
+                        Sale.Sale_State.Available
+                );
+                saleRepository.save(sale);
+            }
+        } catch(Exception e){
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
+        return ResponseEntity.ok(new MessageResponse("Sale created successfully!"));
+    }
+
 
 }
